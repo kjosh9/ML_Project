9b6eaf80b94370bf78f2665a90e21547c158801f
unknown <smatth25@vt.edu>
2016-12-03 17:17:24 -0500

    Modified knn

--- matt_code/knn.m
+++ matt_code/knn.m
@@ -1,25 +1,56 @@
-function predlabels = knn(traindata, trainlabels, testdata, k, f, D)
-    ntrain = size(trainlabels);
+function predlabels = knn(traindata, trainlabels, testdata, k, distFun, weightFun, varargin)
+% Predicts data labels using the k-Nearest Neighbors algorithm.
+% traindata: Data used to train the algorithm. Must be row oriented
+% trainlabels: Labels associated with the training data
+% testdata: The data to be tested. Must have the same second dimension as traindata
+% k: Number of neighbors
+% distFun: The distance function to use with pdist2
+% weightFun: The weighting function to use. Takes a single argument of
+%   distance
+% varargin: Arguments for the distFun
+
+    % Set some useful variables
     ntest = size(testdata, 1);
-    nlabels = 8;
-    predlabels = zeros(ntest, 2);
+    labels = unique(trainlabels);
+    nlabels = size(labels);
+    nlabels = nlabels(1);
     
-    % If D was not given, calculate it using f
-    if (exist('D', 'var')) == 0
-        D = pdist2(traindata, testdata, f);
+    % Preallocate for efficiency
+    predlabels = zeros(ntest, 1);
+    
+    % Check if the weight function was given
+    if exist('weightFun', 'var') == 0
+        weightFun = @(dist) 1;
+    end
+    
+    % Calculate pairwise distances
+    if size(varargin) > 0
+        tic
+        D = pdist2(traindata, testdata, distFun, varargin);
+        toc
+    else
+        tic
+        D = pdist2(traindata, testdata, distFun);
+        toc
     end
     
     % Loop through each test data point
     for i = 1:ntest
-        [neighbors, neighborIndices] = sort(D(:, i));
+        % Sort the distances ascending
+        [dists, indices] = sort(D(:, i));
+        
+        % Preallocate votes array
         votes = zeros(nlabels, 1);
+        
+        % Aggregate votes from k lowest distance neighbors
         for j = 1:k
-            neighborLabel = trainlabels(neighborIndices(j));
-            weight = 1/sqrt(neighbors(j) + .001);
-            %weight = 1/neighbors(j)^k;
-            votes(neighborLabel) = votes(neighborLabel) + weight;
+            neighborLabel = trainlabels(indices(j));
+            weight = weightFun(dists(j));
+            votes(neighborLabel + 1) = votes(neighborLabel + 1) + weight;
         end
+        
+        % Get the predicted label
         [~, label] = max(votes);
-        predlabels(i,:) = [i label];
+        predlabels(i) = label - 1;
     end
 end
