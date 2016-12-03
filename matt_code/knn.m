a7d145dacb6a575cb6d9c8f41d6c99d05beccba5
unknown <smatth25@vt.edu>
2016-12-03 18:58:49 -0500

    Changed arguments for KNN

--- matt_code/knn.m
+++ matt_code/knn.m
@@ -1,37 +1,46 @@
-function predlabels = knn(traindata, trainlabels, testdata, k, distFun, weightFun, varargin)
+function predlabels = knn(traindata, trainlabels, testdata, options)
 % Predicts data labels using the k-Nearest Neighbors algorithm.
 % traindata: Data used to train the algorithm. Must be row oriented
 % trainlabels: Labels associated with the training data
 % testdata: The data to be tested. Must have the same second dimension as traindata
-% k: Number of neighbors
-% distFun: The distance function to use with pdist2
-% weightFun: The weighting function to use. Takes a single argument of
-%   distance
-% varargin: Arguments for the distFun
+% options: Options to change how KNN is run
+%   - distFun: Required if D is not given, function given to pdist2
+%   - k: Number of neighbors to evaluate
+%   - [D]: Precomputed distance matrix
+%   - [weightFun]: Weighting function that takes distance as an argument
 
     % Set some useful variables
     ntest = size(testdata, 1);
     labels = unique(trainlabels);
     nlabels = size(labels);
     nlabels = nlabels(1);
+    k = options.k;
+    if isfield(options, 'weightFun')
+        weightFun = options.weightFun;
+    else
+        weightFun = @(dist) 1;
+    end
+    if isfield(options, 'D')
+        D = options.D;
+    end
+    
     
     % Preallocate for efficiency
     predlabels = zeros(ntest, 1);
     
-    % Check if the weight function was given
-    if exist('weightFun', 'var') == 0
-        weightFun = @(dist) 1;
-    end
-    
     % Calculate pairwise distances
-    if size(varargin) > 0
-        tic
-        D = pdist2(traindata, testdata, distFun, varargin);
-        toc
-    else
-        tic
-        D = pdist2(traindata, testdata, distFun);
-        toc
+    if exist('D', 'var') == 0
+        fprintf('Calculating distances...\n')
+        distFun = options.distFun;
+        if size(varargin) > 0
+            tic
+            D = pdist2(traindata, testdata, distFun, varargin);
+            toc
+        else
+            tic
+            D = pdist2(traindata, testdata, distFun);
+            toc
+        end
     end
     
     % Loop through each test data point
